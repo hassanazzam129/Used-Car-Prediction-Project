ab2eb7a9658c6179c4cd729e7dc832b6cf02a866
hassanazzam129 <107306505+hassanazzam129@users.noreply.github.com>
2023-04-25 21:28:52 +0200

    Add files via upload

---
+++ home.py
@@ -0,0 +1,12 @@
+# importing necessary libraries
+import streamlit as st
+
+# Page content
+st.markdown(" <center>  <h1> Used Car Price Prediction </h1> </font> </center> </h1> ",
+            unsafe_allow_html=True)
+
+st.markdown(''' <center>  <h6>
+    This app is created to predict Used Car Price in Egypt </center> </h6> ''', unsafe_allow_html=True)
+
+# the path of a photo provided from the path of this file
+st.image('Data/Used-Cars-732x384.jpg')
