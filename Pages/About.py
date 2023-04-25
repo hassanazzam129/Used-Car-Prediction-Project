dabacb9cef54154d19f71408f9f5887a31294c94
hassanazzam129 <107306505+hassanazzam129@users.noreply.github.com>
2023-04-25 22:13:14 +0200

    Delete Pages directory

--- Pages/About.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-# Page content
-st.title('About Used Car Price Prediction Project')
-st.write('This app can predict Used Cars Price in egypt the price with accuracy about 91.8%.')
